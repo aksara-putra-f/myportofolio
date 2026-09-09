4ec747f7aaa8a758e61fbd3cf9191b580d5eccac
aksara-putra-f <aksara.putra@ui.ac.id>
2026-09-09 16:09:29 +0700

    feat: Membuat app 'main' yang akan menampilkan page profile dan experience, membuat template menampilkan data secara dinamis

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from main.models import Experience
+
+# Create your views here.
+def show_main(request):
+    context = {
+        "name" : "Aksara Putra Fachruddin",
+        "npm" : "2506597284",
+        "study_program" : "S1 Ilmu Komputer",
+        "bio" : (
+            "My name is Aksara Putra Fachruddin, and I'm a computer science student at Universitas Indonesia ."
+            "Currently, I have an interest in game development and artificial intelligence. "
+            "Throughout my academic years, I've seeking opportunities to learn, grow, and collaborate through personal and team projects, organization, competition, courses, and the technology community."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Aksara Putra Fachruddin",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
