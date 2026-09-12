b9ec0cb43ac50fafa9f4d4ce04c267bcef05af20
aksara-putra-f <aksara.putra@ui.ac.id>
2026-09-13 06:02:00 +0700

    feat: Membuat model data Skill dan template untuk section Skill

--- main/models.py
+++ main/models.py
@@ -30,3 +30,15 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+
+class Skill(models.Model):
+    SKILL_CATEGORY = [
+        ('softskill', 'Softskill'),
+        ('hardskill', 'Hardskill')
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(null=False, blank=False)
+    short_desc = models.CharField(max_length=250, null=False, blank=True)
+    skill_category = models.CharField(choices=SKILL_CATEGORY)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from main.models import Experience
+from main.models import *
 
 # Create your views here.
 def show_main(request):
@@ -22,3 +22,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_skill(request):
+    context = {
+        "name" : "Aksara Putra Fachruddin",
+        "hardskill_list" : Skill.objects.filter(skill_category='hardskill'),
+        "softskill_list" : Skill.objects.filter(skill_category='softskill')
+    }
+    return render(request, "skill.html", context)
